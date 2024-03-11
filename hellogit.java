62ddcc43b6a2784f59fac7ff04bb0485335e8ca1
Dulce <1061684538u.icesi.edu.co>
2024-03-11 17:08:45 -0500

    Este es mi primer commit

---
+++ hellogit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// EJERCICIO NUEMRO 1 
+
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("hola mamá");
+        System.out.println("hola mamá te amo");
+
+    }
+}
