899e57526e6d3e89a8b0164c8f2464ce7eb7aa93
Dulce <1061684538u.icesi.edu.co>
2024-03-11 17:19:29 -0500

    Estos on mis cambios en mi segunda linea

--- hellogit.java
+++ hellogit.java
@@ -6,6 +6,7 @@ public class hellogit {
     public static void main(String[] args) {
         System.out.println("hola mamá");
         System.out.println("hola mamá te amo");
+        System.out.println("al fin pude");
 
     }
 }
